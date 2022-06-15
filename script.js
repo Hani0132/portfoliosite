46d8527e85c188256394ecad11045016062a264b
Hani0132 <hani0132@stud.kea.dk>
2022-06-15 08:07:10 +0200

    ccommit
    alle filer

---
+++ script.js
@@ -0,0 +1,43 @@
+//show more1//
+let moreText = document.querySelector(".more-text");
+
+document.querySelector(".btn").addEventListener("click", showMore);
+
+function showMore() {
+  moreText.classList.toggle("hide");
+}
+// show more 2//
+let moreText2 = document.querySelector(".more-text2");
+
+document.querySelector(".btn2").addEventListener("click", showMore2);
+
+function showMore2() {
+  moreText2.classList.toggle("hide");
+}
+
+// show more 3//
+let moreText3 = document.querySelector(".more-text3");
+
+document.querySelector(".btn3").addEventListener("click", showMore3);
+
+function showMore3() {
+  moreText3.classList.toggle("hide");
+}
+
+// show more 4//
+let moreText4 = document.querySelector(".more-text4");
+
+document.querySelector(".btn4").addEventListener("click", showMore4);
+
+function showMore4() {
+  moreText4.classList.toggle("hide");
+}
+
+// show more 5//
+let moreText5 = document.querySelector(".more-text5");
+
+document.querySelector(".btn5").addEventListener("click", showMore5);
+
+function showMore5() {
+  moreText5.classList.toggle("hide");
+}
